50be17722e11ef2fa3a4c59e1d42edf3f72fc70a
Alexander Bessman <bessman@kth.se>
2015-02-16 16:04:32 +0100

    Remove intruments/ directory. There was nothing of interest there anyway.

--- instruments/keithley/k2xxx.py
+++
@@ -1,7 +0,0 @@
-def keithley2xxx_measure_voltage(instrument):
-    voltage = instrument.ask_for_values('MEASure:VOLTage?')[0]
-    return voltage
-
-def keithley2xxx_measure_current(instrument):
-    current = instrument.ask_for_values('MEASure:CURRent?')[0]
-    return current
